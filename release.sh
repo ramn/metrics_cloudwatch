7639716afeb77dea5859c826bdbf3b0b65554943
Markus Westerlind <markus.westerlind@distilnetworks.com>
2020-08-26 16:04:51 +0200

    Add release script

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+# Usage: ./release.sh patch/minor/major VERSION
+
+
+
+LEVEL=$1
+VERSION=$2
+if [ -z "$LEVEL" ]; then
+    echo "Expected patch, minor or major"
+    exit 1
+fi
+
+clog --$LEVEL
+if [[ -z $(head -1 CHANGELOG.md | grep $VERSION) ]]; then
+    git checkout CHANGELOG.md
+    echo "Wrong version specified"
+    exit 1
+fi
+
+git add CHANGELOG.md
+git commit -m "Update changelog for $VERSION"
+
+cargo release $LEVEL
